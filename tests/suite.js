08657a614580968c986226cc65c3586fb852c838
Stanislav Lesnikov <stan@nashdot.com>
2016-01-31 18:48:34 +0100

    Ident 4 -> 2 spaces

--- tests/suite.js
+++ tests/suite.js
@@ -19,83 +19,82 @@
  * @author Daniel Wirtz <dcode@dcode.io>
  */
 var Preprocessor = require(__dirname+"/../Preprocessor.min.js"),
-    fs = require("fs");
+  fs = require("fs");
 
 var suite = {
-    
-    "init": function(test) {
-        test.ok(typeof Preprocessor == 'function');
-        test.done();
-    },
-    
-    "verbose": function(test) {
-        var pp = new Preprocessor("// #ifdef UNDEFINED\ntest();\n// #endif\n");
-        var msgs = "";
-        pp.process({}, function(msg) { msgs += msg; });
-        test.ok(msgs.length > 0);
-        test.done();
-    },
-    
-    "evaluate": function(test) {
-        var defines = {"VERSION": "1.0"};
-        test.equal('"'+defines["VERSION"]+'";', Preprocessor.evaluate(defines, "'\"'+VERSION+'\";'"));
-        test.equal('"'+"Hello world!"+'";', Preprocessor.evaluate(defines, "\"\\\"Hello world!\\\";\";"));
-        test.equal("2;", Preprocessor.evaluate(defines, "(1+1)+\";\""));
-        test.done();
-    },
-    
-    "test1": function(test) {
-        var pp = new Preprocessor(fs.readFileSync(__dirname+"/test1.js"), __dirname);
-        var src = pp.process({"VERSION": "1.0"}, console.log).replace(/\r/g, "");
-        test.equal(src, '\nconsole.log("UNDEFINED is not defined");\n\nconsole.log("UNDEFINED is not defined (else)");\n\nvar version = "1.0";\n');
-        test.done();
-    },
+  "init": function(test) {
+    test.ok(typeof Preprocessor == 'function');
+    test.done();
+  },
 
-    "test2": function(test) {
-        var pp = new Preprocessor(fs.readFileSync(__dirname+"/test2.js"), __dirname);
-        var src = pp.process({"VERSION": "1.0"}, console.log).replace(/\r/g, "");
-        test.equal(src, '    console.log("2==2")\n    console.log("VERSION=="+"1.0");\n');
-        test.done();
-    },
-    
-    "define": function(test) {
-        var pp = new Preprocessor(fs.readFileSync(__dirname+"/define.js"), __dirname);
-        var src = pp.process({}, console.log).replace(/\r/g, "");
-        test.equal(src, 'var angle = 171.88733853924697;\n');
-        test.done();
-    },
+  "verbose": function(test) {
+    var pp = new Preprocessor("// #ifdef UNDEFINED\ntest();\n// #endif\n");
+    var msgs = "";
+    pp.process({}, function(msg) { msgs += msg; });
+    test.ok(msgs.length > 0);
+    test.done();
+  },
 
-    "include": function(test) {
-        var pp = new Preprocessor("// #include \"number.js\"\n// #include \"number.js\"\n", __dirname),
-            src = pp.process({}, console.log).replace(/\r/g, "");
-        test.strictEqual(src.trim(), '42\n42');
+  "evaluate": function(test) {
+    var defines = {"VERSION": "1.0"};
+    test.equal('"'+defines["VERSION"]+'";', Preprocessor.evaluate(defines, "'\"'+VERSION+'\";'"));
+    test.equal('"'+"Hello world!"+'";', Preprocessor.evaluate(defines, "\"\\\"Hello world!\\\";\";"));
+    test.equal("2;", Preprocessor.evaluate(defines, "(1+1)+\";\""));
+    test.done();
+  },
 
-        pp = new Preprocessor("// #include_once \"number.js\"\n// #include_once \"number.js\"\n", __dirname);
-        src = pp.process({}, console.log).replace(/\r/g, "");
-        test.strictEqual(src.trim(), '42');
+  "test1": function(test) {
+    var pp = new Preprocessor(fs.readFileSync(__dirname+"/test1.js"), __dirname);
+    var src = pp.process({"VERSION": "1.0"}, console.log).replace(/\r/g, "");
+    test.equal(src, '\nconsole.log("UNDEFINED is not defined");\n\nconsole.log("UNDEFINED is not defined (else)");\n\nvar version = "1.0";\n');
+    test.done();
+  },
 
-        test.done();
-    },
+  "test2": function(test) {
+    var pp = new Preprocessor(fs.readFileSync(__dirname+"/test2.js"), __dirname);
+    var src = pp.process({"VERSION": "1.0"}, console.log).replace(/\r/g, "");
+    test.equal(src, '    console.log("2==2")\n    console.log("VERSION=="+"1.0");\n');
+    test.done();
+  },
 
-    "includeGlob": function(test) {
-        var pp = new Preprocessor("// #include_once \"subdir/*.js\"\n", __dirname),
-            src = pp.process({}, console.log).replace(/\r/g, "");
-        test.equal(src.trim(), "'glob1.js';'glob2.js';");
+  "define": function(test) {
+    var pp = new Preprocessor(fs.readFileSync(__dirname+"/define.js"), __dirname);
+    var src = pp.process({}, console.log).replace(/\r/g, "");
+    test.equal(src, 'var angle = 171.88733853924697;\n');
+    test.done();
+  },
 
-        pp = new Preprocessor("// #include_once \"subdir/**/glob*.js\"\n", __dirname);
-        src = pp.process({}, console.log).replace(/\r/g, "");
-        test.equal(src.trim(), "'glob1.js';'glob2.js';'glob3.js';");
+  "include": function(test) {
+    var pp = new Preprocessor("// #include \"number.js\"\n// #include \"number.js\"\n", __dirname),
+      src = pp.process({}, console.log).replace(/\r/g, "");
+    test.strictEqual(src.trim(), '42\n42');
 
-        test.done();
-    },
+    pp = new Preprocessor("// #include_once \"number.js\"\n// #include_once \"number.js\"\n", __dirname);
+    src = pp.process({}, console.log).replace(/\r/g, "");
+    test.strictEqual(src.trim(), '42');
 
-	"preserveLineNumbers": function(test) {
-		var pp = new Preprocessor(fs.readFileSync(__dirname+"/preserve_lines.js"), __dirname, true);
-		var src = pp.process({}, console.log).replace(/\r/g, "");
-		test.equal(src, "var i = 0;\n\n\n\ni = 2;\n\n\nconsole.log(i);\n");
+    test.done();
+  },
 
-		test.done();
-	}
+  "includeGlob": function(test) {
+    var pp = new Preprocessor("// #include_once \"subdir/*.js\"\n", __dirname),
+      src = pp.process({}, console.log).replace(/\r/g, "");
+    test.equal(src.trim(), "'glob1.js';'glob2.js';");
+
+    pp = new Preprocessor("// #include_once \"subdir/**/glob*.js\"\n", __dirname);
+    src = pp.process({}, console.log).replace(/\r/g, "");
+    test.equal(src.trim(), "'glob1.js';'glob2.js';'glob3.js';");
+
+    test.done();
+  },
+
+  "preserveLineNumbers": function(test) {
+    var pp = new Preprocessor(fs.readFileSync(__dirname+"/preserve_lines.js"), __dirname, true);
+    var src = pp.process({}, console.log).replace(/\r/g, "");
+    test.equal(src, "var i = 0;\n\n\n\ni = 2;\n\n\nconsole.log(i);\n");
+
+    test.done();
+  }
 };
 
 module.exports = suite;
