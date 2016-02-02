628e14a8d68adfca766180c05340ae398a812645
Stanislav Lesnikov <stan@nashdot.com>
2016-02-02 08:58:43 +0100

    Global test run

---
+++ tests/run.js
@@ -0,0 +1,14 @@
+
+require('./test_01.js');
+require('./test_02.js');
+require('./test_03.js');
+require('./test_04.js');
+require('./test_05.js');
+require('./test_06_01.js');
+require('./test_06_02.js');
+require('./test_06_03.js');
+require('./test_07_01.js');
+require('./test_07_02.js');
+require('./test_08.js');
+require('./test_09_01.js');
+require('./test_09_02.js');
